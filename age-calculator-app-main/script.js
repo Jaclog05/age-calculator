cd1fcd566beaea6b099830195ba3da727573e964
Jaider <jaidercueto.1010@gmail.com>
2023-05-04 14:11:42 -0500

    exact date

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -8,11 +8,34 @@ const rMonths = document.getElementById('m')
 const rDays = document.getElementById('d')
 
 submitEl.addEventListener('click', () => {
-    const untilBirthDay = Date.parse(`${year.value}-${month.value}-${day.value}`)
-    const untilNow = Date.now()
-    const secondsSinceDate = Math.floor((untilNow - untilBirthDay) / 1000)
 
-    rYears.innerHTML = `${Math.floor(secondsSinceDate / 31536000)}`
-    rMonths.innerHTML = `${Math.floor((secondsSinceDate % 31536000) / 2635200)}`
-    rDays.innerHTML = `${Math.floor(((secondsSinceDate % 31536000) % 2635200) / 86400)}`
+    const currentDate = new Date()
+    const currentDay = currentDate.getDay()
+    const currentMonth = currentDate.getMonth() + 1
+    const currentYear = currentDate.getFullYear()
+
+    const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if(currentDay >= day.value){
+        if(currentMonth >= month.value){
+            rDays.innerHTML = `${currentDay - day.value}`
+            rMonths.innerHTML = `${currentMonth - month.value}`
+            rYears.innerHTML = `${currentYear - year.value}`
+        }else{
+            rDays.innerHTML = `${currentDay - day.value}`
+            rMonths.innerHTML = `${12 - Math.abs(currentMonth - month.value)}`
+            rYears.innerHTML = `${currentYear - year.value - 1}`
+        }
+
+    }else if(currentDay < day.value){
+        if(currentMonth > month.value){
+            rDays.innerHTML = `${daysInMonth[month.value - 1] - Math.abs(currentDay - day.value)}`
+            rMonths.innerHTML = `${Math.abs(currentMonth - month.value) - 1}`
+            rYears.innerHTML = `${currentYear - year.value}`
+        }else {
+            rDays.innerHTML = `${daysInMonth[month.value - 1] - Math.abs(currentDay - day.value)}`
+            rMonths.innerHTML = `${12 - Math.abs(currentMonth - month.value) - 1}`
+            rYears.innerHTML = `${currentYear - year.value - 1}`
+        }
+    }
 })
