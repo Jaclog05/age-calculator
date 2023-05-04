cc87db8811670b7e5e57aab5fde2c29c1852559b
Jaider <jaidercueto.1010@gmail.com>
2023-05-04 16:28:03 -0500

    validation function added - future dates, valid days and months - required fields

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -9,6 +9,8 @@ const rDays = document.getElementById('d')
 
 submitEl.addEventListener('click', () => {
 
+    validation()
+
     const currentDate = new Date()
     const currentDay = currentDate.getDay()
     const currentMonth = currentDate.getMonth() + 1
@@ -16,7 +18,7 @@ submitEl.addEventListener('click', () => {
 
     const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    if(currentDay >= day.value){
+    if(validation() && currentDay >= day.value){
         if(currentMonth >= month.value){
             rDays.innerHTML = `${currentDay - day.value}`
             rMonths.innerHTML = `${currentMonth - month.value}`
@@ -27,7 +29,7 @@ submitEl.addEventListener('click', () => {
             rYears.innerHTML = `${currentYear - year.value - 1}`
         }
 
-    }else if(currentDay < day.value){
+    }else if(validation() && currentDay < day.value){
         if(currentMonth > month.value){
             rDays.innerHTML = `${daysInMonth[month.value - 1] - Math.abs(currentDay - day.value)}`
             rMonths.innerHTML = `${Math.abs(currentMonth - month.value) - 1}`
@@ -39,3 +41,43 @@ submitEl.addEventListener('click', () => {
         }
     }
 })
+
+const validation = () => {
+
+    const pElements = document.querySelectorAll('h6')
+    const errorYearEl = document.getElementById('errorYear')
+    const errorMonthEl = document.getElementById('errorMonth')
+    const errorDayEl = document.getElementById('errorDay')
+    const enteredDate = Date.parse(`${year.value}-${month.value}-${day.value}`)
+    const currentDate = Date.now()
+
+    if(!day.value || !month.value || !year.value){
+
+        pElements.forEach(p => {
+            p.innerText = 'This field is required'
+            p.parentElement.classList.add('errorClass')
+
+        })
+        return false
+    }else {
+        pElements.forEach(p => {
+            p.parentElement.classList.remove('errorClass')
+            p.innerHTML = ''
+        })
+        if(enteredDate > currentDate){
+            errorYearEl.parentElement.classList.add('errorClass')
+            errorYearEl.innerText = 'Must be in the past'
+            return false
+        }else if(day.value > 31 || day.value <= 0){
+            errorDayEl.parentElement.classList.add('errorClass')
+            errorDayEl.innerText = 'Must be a valid day'
+            return false
+        }else if(month.value > 12 || month.value <= 0){
+            errorMonthEl.parentElement.classList.add('errorClass')
+            errorMonthEl.innerText = 'Must be a valid month'
+            return false
+        }else {
+            return true
+        }
+    }
+}
